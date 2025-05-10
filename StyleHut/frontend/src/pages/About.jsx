d8f36c5638c5020ae4c10356cd5e6e654c11c832
zetacoding3 <zetacoding.academics@gmail.com>
2025-05-10 18:12:38 +0530

    initial commit

---
+++ StyleHut/frontend/src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <div className="container-fluid g-0 px-3 py-5">
+                <img src="/img/about.webp" alt="" className="img-fluid col-md-4 float-start mx-4" />
+                <h1 className="text-center fw-semibold">Our Story</h1>
+                <p className='fs-5'>
+                    StyleHut is your go-to destination for trendy fashion and apparel that speaks confidence, class, and comfort. Whether you're chasing the latest trends or curating your signature look, we bring you a curated collection of stylish clothing, accessories, and footwear to elevate your wardrobe.
+                </p>
+                <p className='fs-5'>
+                    At StyleHut, fashion is more than fabric — it’s an expression of identity. Our pieces are handpicked for their quality, design, and seasonal relevance, ensuring you always stay ahead of the curve. From street style to chic casuals, and modern formals to effortless everyday wear — we’ve got it all.
+                </p>
+                <p className='fs-5'>
+                    We are committed to offering affordable fashion without compromising on quality. With easy online shopping, secure payments, and fast delivery, StyleHut makes it simple for you to look good and feel great.
+                </p>
+                <p className='fs-5'>
+                    Discover your style. Define your vibe. Only at StyleHut.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
